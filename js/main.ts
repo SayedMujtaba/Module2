1aeec944dc936f0de7303c9ad32f721e2f4a18b7
Sayed <s.m.mirzad@gmail.com>
2016-08-19 00:40:15 +1200

    Design for speech api. changeing btn and a textfiled

--- js/main.ts
+++ js/main.ts
@@ -1,14 +1,16 @@
 var record = $("#record")[0];
 var ricon = $("#recordIcon")[0];
 var toutput = $("toutput")[0];
-record.onclick = function chgColor() {
+
+record.onclick = function chgColor():void{
     console.log(record.className);
-    if (record.className == 'btn btn-success') {
+    if(record.className == 'btn btn-success'){
         record.className = "btn btn-danger";
         ricon.className = "glyphicon glyphicon-stop";
-    }
-    else {
+    }else{
         record.className = "btn btn-success";
         ricon.className = "glyphicon glyphicon-record";
+        
     }
-};
+   
+}
