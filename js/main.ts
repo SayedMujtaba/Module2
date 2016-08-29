928c3bc7aae7369a9a49ee1367e5f1a64c8e6257
Sayed <s.m.mirzad@gmail.com>
2016-08-29 22:01:04 +1200

    conneted to Omdb. Form is working now and getting proper response.

--- js/main.ts
+++ js/main.ts
@@ -1,48 +1,24 @@
-var record = $("#record")[0];
-var ricon = $("#recordIcon")[0];
-var toutput = $("#toutput")[0];
+var form = $("#searchByTitle");
+var btnSearch = $("#btnSearch-title")[0];
+var btnReset = $("#btnReset-title")[0];
 
-record.onclick = function chgColor():void{
-    console.log(record.className);
-    if(record.className == 'btn btn-success'){
-        record.className = "btn btn-danger";
-        ricon.className = "glyphicon glyphicon-stop";
-        sendOmdbRequest(function(movieData){
-            toutput.innerHTML =  movieData.Year;
-        });
-    }else{
-        record.className = "btn btn-success";
-        ricon.className = "glyphicon glyphicon-record";
-        
-    }
-   
+btnSearch.onclick = function ():void{
+    var userdata = form.serialize();
+    sendOmdbRequest(userdata, function(omdbResult){
+    console.log(omdbResult);
+    });
 }
 
-function sendOmdbRequest(callback) : void {
+function sendOmdbRequest(userdata, callback) : void {
     $.ajax({
-        url: "http://www.omdbapi.com/?t=Another",
-       /* beforeSend: function (xhrObj) {
-            // Request headers
-            xhrObj.setRequestHeader("Content-Type", "application/octet-stream");
-            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "d342c8d19d4e4aafbf64ed9f025aecc8");
-        },*/
+        url: "http://www.omdbapi.com/?"+ userdata,
         type: "Get",
         data: Text,
         processData: false
-    })
-        .done(function (data) {
-            if (data.length != 0) { // if a movie is detected
-                // Get the emotion scores
-                var movie = data;
-                callback(movie);
-                console.log(movie);
-                
-            } else {
-                toutput.innerHTML = " Try another?";
-            }
-        })
-        .fail(function (error) {
-            toutput.innerHTML = "Sorry, something went wrong. :( Try again in a bit?";
+    }).done(function (data) {
+           var movie = data;
+           callback(movie); 
+        }).fail(function (error) {
             console.log(error.getAllResponseHeaders());
         });
 }
